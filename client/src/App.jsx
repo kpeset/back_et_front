8ff49d283fab8cb1e77d2c202019a019aaf2d26f
Kevin Peset <97118056+kpeset@users.noreply.github.com>
2024-07-22 17:05:38 +0200

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <h1>Mon template</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda sunt
+        quisquam est nulla repudiandae ullam adipisci cupiditate modi non,
+        recusandae corporis corrupti blanditiis voluptates perspiciatis
+        voluptatibus natus ipsa iusto eos?
+      </p>
+    </>
+  );
+}
+
+export default App;
